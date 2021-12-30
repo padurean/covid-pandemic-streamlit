3bfe88f9adb3b4c5539fc18025f2ff2232fc851d
Valentin Padurean (Ogg) <912518+padurean@users.noreply.github.com>
2021-12-30 17:09:25 +0200

    some minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,20 +20,18 @@ df = load_data()
 # <-- data
 
 # --> user inputs and filtered data
-# countries multiselect input
-all_countries = df[LOCATION_COLUMN].unique()
-countries = st.sidebar.multiselect("Countries", all_countries, default=['Germany', 'Sweden', 'Romania'])
+# locations multiselect input
+all_locations = df[LOCATION_COLUMN].unique()
+locations = st.sidebar.multiselect('Locations', all_locations, default=['Germany', 'Sweden', 'Romania'])
+if not locations:
+  ':no_entry: Please select at least one location.'
+  st.stop()
 
 # filter data
-df = df[df[LOCATION_COLUMN].isin(countries)]
-# df
+df = df[df[LOCATION_COLUMN].isin(locations)]
+
 df_deaths = df[[DATE_COLUMN,LOCATION_COLUMN,NEW_DEATHS_PER_MILLION_COLUMN,NEW_DEATHS_COLUMN]]
-df_deaths = df_deaths[
-  # df_deaths[DATE_COLUMN].notnull() &
-  df_deaths[NEW_DEATHS_PER_MILLION_COLUMN].notnull()
-  # df_deaths[NEW_DEATHS_COLUMN].notnull()
-]
-# df_deaths
+df_deaths = df_deaths[df_deaths[NEW_DEATHS_PER_MILLION_COLUMN].notnull()]
 
 # dates range input
 dates = sorted(pd.to_datetime(df_deaths[DATE_COLUMN]).dt.date.unique().tolist())
@@ -41,8 +39,8 @@ start_date, end_date = st.sidebar.select_slider(
   'Show for period',
   options=dates,
   value=(dates[-200], dates[-1]),
-  format_func=(lambda x: x.strftime("%Y/%m")))
-# <-- user inputs
+  format_func=(lambda x: x.strftime("%b'%y")))
+# <-- user inputs and filtered data
 
 # --> chart
 df_deaths_in_range = df_deaths[
@@ -50,18 +48,23 @@ df_deaths_in_range = df_deaths[
   (df_deaths[DATE_COLUMN] <= pd.Timestamp(end_date))
 ]
 deaths_lines = alt.Chart().mark_line(point=True).encode(
-  x=alt.X(DATE_COLUMN, title="Date"),
+  x=alt.X('monthdate('+DATE_COLUMN+'):O', title='Date'),
   y=alt.Y(NEW_DEATHS_PER_MILLION_COLUMN, title='New Deaths per Million'),
-  color=alt.Color(LOCATION_COLUMN, title='Country'),
-  tooltip=[DATE_COLUMN, LOCATION_COLUMN, NEW_DEATHS_PER_MILLION_COLUMN, NEW_DEATHS_COLUMN]
+  color=alt.Color(LOCATION_COLUMN, title='Location'),
+  tooltip=[
+    alt.Tooltip(DATE_COLUMN, title='Date'),
+    alt.Tooltip(LOCATION_COLUMN, title='Location'),
+    alt.Tooltip(NEW_DEATHS_COLUMN, title='New Deaths'),
+    alt.Tooltip(NEW_DEATHS_PER_MILLION_COLUMN, title='New Deaths per Million'),
+  ]
 )
 deaths_mean_lines = alt.Chart().mark_rule(strokeDash=[5,1]).encode(
   y='mean('+NEW_DEATHS_PER_MILLION_COLUMN+')',
   color=LOCATION_COLUMN,
   size=alt.SizeValue(2),
   tooltip=[
-    alt.Tooltip('mean('+NEW_DEATHS_PER_MILLION_COLUMN+')', title="Mean"),
-    alt.Tooltip(LOCATION_COLUMN, title="Country")
+    alt.Tooltip('mean('+NEW_DEATHS_PER_MILLION_COLUMN+')', title='Mean'),
+    alt.Tooltip(LOCATION_COLUMN, title='Location')
   ])
 chart = alt.layer(
   deaths_lines,
