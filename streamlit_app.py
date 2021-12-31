6a94c647f24b25a718ae0b3caea50056bf088e7f
Valentin Padurean (Ogg) <912518+padurean@users.noreply.github.com>
2021-12-31 15:36:14 +0200

    show both deaths and cases

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,73 +2,139 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-DATE_COLUMN = 'date'
-LOCATION_COLUMN = 'location'
-NEW_DEATHS_COLUMN = 'new_deaths'
-NEW_DEATHS_PER_MILLION_COLUMN = 'new_deaths_per_million'
+# column names constants
+DATE_COL = 'date'
+LOCATION_COL = 'location'
+# deaths columns
+NEW_DEATHS_COL = 'new_deaths'
+NEW_DEATHS_PER_MILLION_COL = 'new_deaths_per_million'
+NEW_DEATHS_SMOOTHED_PER_MILLION_COL = 'new_deaths_smoothed_per_million'
+# cases columns
+NEW_CASES_COL = 'new_cases'
+NEW_CASES_PER_MILLION_COL = 'new_cases_per_million'
+NEW_CASES_SMOOTHED_PER_MILLION_COL = 'new_cases_smoothed_per_million'
 
+# page header
 st.markdown(
   """### COVID Pandemic :mag_right: Visual Data Explorer
   <small>_data is refreshed each 3 hours from
   [OurWorldInData.org](https://github.com/owid/covid-19-data/tree/master/public/data)_</small>""",
   unsafe_allow_html=True)
 
-# --> data
-# cache the data for 3 hours
+#===> functions definitions
+# loads the data (cached for 3 hours)
 @st.experimental_memo(ttl=3*60*60)
 def load_data():
-  return pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv', parse_dates=[DATE_COLUMN])
+  return pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv', parse_dates=[DATE_COL])
 
+# create and shows a chart
+def show_rel_chart(kind, rel_unit, df, smoothed, show_mean, abs_col, rel_col, rel_col_smoothed):
+  # configure column and labels depending on the smoothing option
+  col = rel_col_smoothed
+  smoothed_label = ' (smoothed)'
+  if not smoothed:
+    col = rel_col
+    smoothed_label = ''
+
+  abs_title = 'New '+ kind
+  rel_title = abs_title +' per ' + rel_unit
+  smoothed_title = rel_title + ' (smoothed)'
+  title = rel_title + smoothed_label
+
+  # data lines
+  lines = alt.Chart(title=title).mark_line(point=True).encode(
+    x=alt.X('monthdate('+DATE_COL+'):O', title=''),
+    y=alt.Y(col, title=title),
+    color=alt.Color(LOCATION_COL, title='Location'),
+    tooltip=[
+      alt.Tooltip(DATE_COL, title='Date'),
+      alt.Tooltip(LOCATION_COL, title='Location'),
+      alt.Tooltip(abs_col, title=abs_title),
+      alt.Tooltip(rel_col, title=rel_title),
+      alt.Tooltip(rel_col_smoothed, title=smoothed_title),
+    ])
+  all_lines = [lines]
+
+  # data mean lines
+  if show_mean:
+    mean_lines = alt.Chart().mark_rule(strokeDash=[5,1]).encode(
+      y='mean('+rel_col+')',
+      color=LOCATION_COL,
+      size=alt.SizeValue(2),
+      tooltip=[
+        alt.Tooltip('mean('+rel_col+')', title='Mean (not smoothed)'),
+        alt.Tooltip(LOCATION_COL, title='Location')
+      ])
+    all_lines.append(mean_lines)
+
+  # create and show chart with all lines
+  chart = alt.layer(*all_lines, data=df).configure_legend(orient='bottom')
+  st.altair_chart(chart, use_container_width=True)
+#<===
+
+#===> main script
+# load data
 df = load_data()
-# <--
 
-# --> user inputs and filtered data
-# locations multiselect input
-all_locations = df[LOCATION_COLUMN].unique()
-locations = st.multiselect('Show for locations:', all_locations, default=['Germany', 'Sweden', 'Romania'])
-if not locations:
-  ':no_entry: Please select at least one location.'
-  st.stop()
+#--> show user inputs and filter data
+config_expander = st.expander('⚙️ Configure', expanded=False)
+
+all_locations = sorted(df[LOCATION_COL].unique())
+with config_expander:
+  # data kind
+  kind = st.selectbox("Data:", ('deaths', 'cases'), index=0, format_func=lambda x: x.capitalize())
+  # locations multiselect input
+  locations = st.multiselect('Locations:', all_locations, default=['Germany', 'Sweden', 'Romania'])
+  if not locations:
+    ':no_entry: Please select at least one location.'
+    st.stop()
 
 # filter data
-df = df[df[LOCATION_COLUMN].isin(locations)]
+abs_col=NEW_DEATHS_COL
+rel_col=NEW_DEATHS_PER_MILLION_COL
+rel_col_smoothed=NEW_DEATHS_SMOOTHED_PER_MILLION_COL
+if kind == 'cases':
+  abs_col=NEW_CASES_COL
+  rel_col=NEW_CASES_PER_MILLION_COL
+  rel_col_smoothed=NEW_CASES_SMOOTHED_PER_MILLION_COL
 
-df_deaths = df[[DATE_COLUMN,LOCATION_COLUMN,NEW_DEATHS_PER_MILLION_COLUMN,NEW_DEATHS_COLUMN]]
-df_deaths = df_deaths[df_deaths[NEW_DEATHS_PER_MILLION_COLUMN].notnull()]
+df = df[df[LOCATION_COL].isin(locations)]
+df_sub = df[[DATE_COL,LOCATION_COL,abs_col,rel_col,rel_col_smoothed]]
+df_sub = df_sub[df_sub[rel_col].notnull()]
 
-# dates range input
-dates = sorted(pd.to_datetime(df_deaths[DATE_COLUMN]).dt.date.unique().tolist())
-start_date, end_date = st.select_slider(
-  'Show for period:',
-  options=dates,
-  value=(dates[-200], dates[-1]),
-  format_func=(lambda x: x.strftime("%b'%y")))
-# <--
+dates = sorted(pd.to_datetime(df_sub[DATE_COL]).dt.date.unique().tolist())
+with config_expander:
+  # dates range input
+  start_date, end_date = st.select_slider(
+    'Period:',
+    options=dates,
+    value=(dates[-90], dates[-1]),
+    format_func=(lambda x: x.strftime("%b'%y")))
 
-# --> chart
-df_deaths_in_range = df_deaths[
-  (df_deaths[DATE_COLUMN] >= pd.Timestamp(start_date)) &
-  (df_deaths[DATE_COLUMN] <= pd.Timestamp(end_date))
+  col1, col2, _ = st.columns(3)
+  with col1:
+    # 7-day smoothed option
+    smoothed = st.checkbox('7-day smoothed', value=True)
+  with col2:
+    # show/hide mean option
+    show_mean = st.checkbox('Show mean', value=True)
+#<--
+
+#--> filter data and show charts
+# filter data for selected dates
+df_in_range = df_sub[
+  (df_sub[DATE_COL] >= pd.Timestamp(start_date)) &
+  (df_sub[DATE_COL] <= pd.Timestamp(end_date))
 ]
-deaths_lines = alt.Chart(title="New Deaths per Million").mark_line(point=True).encode(
-  x=alt.X('monthdate('+DATE_COLUMN+'):O', title=''),
-  y=alt.Y(NEW_DEATHS_PER_MILLION_COLUMN, title='New Deaths per Million'),
-  color=alt.Color(LOCATION_COLUMN, title='Location'),
-  tooltip=[
-    alt.Tooltip(DATE_COLUMN, title='Date'),
-    alt.Tooltip(LOCATION_COLUMN, title='Location'),
-    alt.Tooltip(NEW_DEATHS_COLUMN, title='New Deaths'),
-    alt.Tooltip(NEW_DEATHS_PER_MILLION_COLUMN, title='New Deaths per Million'),
-  ])
-deaths_mean_lines = alt.Chart().mark_rule(strokeDash=[5,1]).encode(
-  y='mean('+NEW_DEATHS_PER_MILLION_COLUMN+')',
-  color=LOCATION_COLUMN,
-  size=alt.SizeValue(2),
-  tooltip=[
-    alt.Tooltip('mean('+NEW_DEATHS_PER_MILLION_COLUMN+')', title='Mean'),
-    alt.Tooltip(LOCATION_COLUMN, title='Location')
-  ])
-chart = alt.layer(deaths_lines, deaths_mean_lines, data=df_deaths_in_range).configure_legend(orient='bottom')
-
-st.altair_chart(chart, use_container_width=True)
-# <--
+# show the relative chart
+show_rel_chart(
+  kind=kind,
+  rel_unit='million',
+  df=df_in_range,
+  smoothed=smoothed,
+  show_mean=show_mean,
+  abs_col=abs_col,
+  rel_col=rel_col,
+  rel_col_smoothed=rel_col_smoothed)
+#<--
+#<===
